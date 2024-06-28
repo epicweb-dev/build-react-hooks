69bdd251e27745c415c3a3c18e173a908aa0d132
Kent C. Dodds <me+github@kentcdodds.com>
2024-06-28 09:30:52 -0600

    init

---
+++ exercises/01.example/01.problem.hello/index.js
@@ -0,0 +1,8 @@
+import { createServer } from 'http'
+
+const server = createServer((req, res) => {
+	res.writeHead(200, { 'Content-Type': 'text/plain' })
+	res.end('hello world')
+})
+
+server.listen(process.env.PORT)
